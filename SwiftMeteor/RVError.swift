3c742ed737c03559fe7c33b76e425574776396a1
Neil Weintraut <neil@Neils-iMac.local>
2016-11-29 08:46:45 -0800

    Middle of Updating RVAWS

---
+++ SwiftMeteor/RVError.swift
@@ -0,0 +1,56 @@
+//
+//  RVError.swift
+//  SwiftMeteor
+//
+//  Created by Neil Weintraut on 11/29/16.
+//  Copyright © 2016 Neil Weintraut. All rights reserved.
+//
+
+//
+//  RVError.swift
+//  rendevu
+//
+//  Created by Neil Weintraut on 7/15/16.
+//  Copyright © 2016 Neil Weintraut. All rights reserved.
+//
+
+import UIKit
+
+
+class RVError: NSError {
+    static var domain: String = "RV"
+    var sourceError: NSError? = nil
+    var messages = [String]()
+    var fileName: String = ""
+    var functionName: String = ""
+    var time = NSDate()
+    var lineNumber: Int = -1
+    
+    init(message: String, sourceError: NSError? = nil) {
+        super.init(domain: RVError.domain, code: 0, userInfo: ["message": message])
+        self.messages = [message]
+        if let error = sourceError { self.sourceError = error }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    func append(message: String) {
+        self.messages.insert(message, atIndex: 0)
+    }
+    func output() -> String {
+        var messages = ""
+        for message in self.messages.enumerate() {
+            messages += "\(message)\n"
+        }
+        var output = "--- at \(RVDateFormatter.ddHHmmsssss.stringFromDate(time)) Error in \(fileName)\(functionName).\(lineNumber)\n\(messages)"
+        if let error = self.sourceError {
+            output += "\(error)"
+        }
+        output += "\n------------------------- End Error Message -----------------------------------"
+        return output
+    }
+    func printError() {
+        print(output())
+    }
+}
