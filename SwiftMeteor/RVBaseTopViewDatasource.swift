fbf012120647efec26830c3fd1ea977c48d46cfc
Neil Weintraut <neil@Neils-iMac.local>
2017-03-18 15:36:06 -0700

    Wproking. Search Next

--- SwiftMeteor/RVBaseTopViewDatasource.swift
+++ SwiftMeteor/RVBaseTopViewDatasource.swift
@@ -8,13 +8,19 @@
 
 import Foundation
 class RVBaseTopViewDatasource: RVBaseDatasource2 {
+    var FakeAnEntry: Bool = true
     override func basicQuery() -> RVQuery {
         let query = RVQuery()
         query.limit = 70
         return query
     }
     override func bulkQuery(query: RVQuery, callback: @escaping (_ models: [RVBaseModel]?, _ error: RVError?) -> Void) {
-        callback([RVBaseModel](), nil)
+        if self.array.count == 0 && FakeAnEntry {
+            callback([RVBaseModel()], nil)
+        } else {
+            callback([RVBaseModel](), nil)
+        }
+
     }
 
 }
